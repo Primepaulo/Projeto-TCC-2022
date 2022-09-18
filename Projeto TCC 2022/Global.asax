94d161f448f1d21ede5fb7a3204b85ee8cbdd377
Paulo <paulovsgomes@gmail.com>
2022-09-18 15:58:04 -0300

    Adicionar arquivos de projeto.

---
+++ Projeto TCC 2022/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Projeto_TCC_2022.MvcApplication" Language="C#" %>
---
+++ Projeto TCC 2022/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Projeto_TCC_2022.Startup))]
+namespace Projeto_TCC_2022
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
