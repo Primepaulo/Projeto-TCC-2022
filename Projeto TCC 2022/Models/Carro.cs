5ca5f715623454ccb2bd3acb577dc15cf25f2226
Magno <magnoramosguimaraes@gmail.com>
2022-09-20 14:13:34 -0300

    Adicionada classe carro.

---
+++ Projeto TCC 2022/Models/Carro.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Projeto_TCC_2022.Models
+{
+    public class Carro
+    {
+        private readonly static string _conn = @"";
+
+        public string Placa { get; set; }
+        public string Cor { get; set; }
+        public string Modelo { get; set; }
+        public decimal Motorização { get; set; }
+        public string Marca { get; set; }
+        public int Fk_Pessoa_Id { get; set; }
+
+        public Carro(){}
+
+        public Carro(string placa, string cor, string modelo, decimal motorização, string marca, int fk_Pessoa_Id)
+        {
+            Placa = placa;
+            Cor = cor;
+            Modelo = modelo;
+            Motorização = motorização;
+            Marca = marca;
+            Fk_Pessoa_Id = fk_Pessoa_Id;
+
+        }
+        
+
+
+
+
+    }
+}
