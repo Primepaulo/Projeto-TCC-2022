8fb3e5b61fb10f940ce07fc0b6767b8019734d99
Magno <magnoramosguimaraes@gmail.com>
2022-09-27 15:06:30 -0300

    adicionado pessoa model :thumb_up:

---
+++ Projeto TCC 2022/Models/PessoaModels.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+
+namespace Projeto_TCC_2022.Models
+{
+    public class PessoaModels
+    {
+        private readonly static string _conn = @"Data Source=EN2D09466CE6722\SQLEXPRESS;Initial Catalog = TdIdD(IN305); Integrated Security = True; Connect Timeout = 30; Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+        public string Estado { get; set; }
+        public string Cidade { get; set; }
+        public string Rua { get; set; }
+        public int Número { get; set; }
+        public string Complemento { get; set; }
+        public int fk_CelularTelefone_Id { get; set; }
+        public string EMail { get; set; }
+        public string CPF { get; set; }
+        public string CNPJ { get; set; }
+        public int Pessoa_Tipo { get; set; }
+
+
+
+        public PessoaModels(){}
+
+        public PessoaModels(string placa, string cor, string modelo, decimal motorização, string marca, int fk_Pessoa_Id)
+        {
+            Placa = placa;
+            Cor = cor;
+            Modelo = modelo;
+            Motorização = motorização;
+            Marca = marca;
+            Fk_Pessoa_Id = fk_Pessoa_Id;
+
+        }
+
+        
+
+        
+    }
+}
