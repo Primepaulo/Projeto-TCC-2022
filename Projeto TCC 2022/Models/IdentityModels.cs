db417118b9bacfbc7532d8eaf5d0b6fb3c1eb26d
Magno <magnoramosguimaraes@gmail.com>
2022-12-06 14:12:00 -0300

    .

--- Projeto TCC 2022/Models/IdentityModels.cs
+++ Projeto TCC 2022/Models/IdentityModels.cs
@@ -51,7 +51,7 @@ namespace Projeto_TCC_2022.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser, CustomRole, int, CustomUserLogin, CustomUserRole, CustomUserClaim>
     {
         public ApplicationDbContext()
-            : base("BibliotecaPonta")
+            : base("LAB3PAULO")
         {
         }
 
--- Projeto TCC 2022/Models/Model1.cs
+++ Projeto TCC 2022/Models/Model1.cs
@@ -29,7 +29,7 @@ namespace Projeto_TCC_2022.Models
     public partial class Model1 : DbContext
     {
         public Model1()
-            : base("name=BibliotecaPonta" /*is on Web.config file at line 12 in connectionString*/)
+            : base("name=LAB3PAULO" /*is on Web.config file at line 12 in connectionString*/)
         //Trocar também no IdentityModels.
         {
         }
